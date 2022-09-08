be282922b9f0183b3db7a34d3aa8926ad81a1b87
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-08 09:58:53 +0530

    Define target elements

---
+++ app.js
@@ -0,0 +1,20 @@
+//psuedo code
+
+// 1. define target elements - buttons, score
+// 2. define count for home, count for guest
+// 3. onclick() - define functions in html
+// 4. add points logic in the functions accordingly
+
+// 1.
+
+const homeScore = document.getElementById("home-score");
+const guestScore = document.getElementById("guest-score");
+
+const plusOneforHome = document.getElementById("plus-one-home");
+const plusTwoforHome = document.getElementById("plus-one-home");
+const plusThreeforHome = document.getElementById("plus-one-home");
+
+const plusOneforGuest = document.getElementById("plus-one-home");
+const plusTwoforGuest = document.getElementById("plus-one-home");
+const plusThreeforGuest = document.getElementById("plus-one-home");
+
