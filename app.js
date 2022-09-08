cfd663dd171c97456ceabf6f6ac12119beeb7198
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-08 10:08:53 +0530

    Add functions for click action

--- app.js
+++ app.js
@@ -20,3 +20,34 @@ const plusThreeforGuest = document.getElementById("plus-one-guest");
 
 let countHome = 0;
 let countGuest = 0;
+
+function plusOneHome() {
+    // console.log("im in");
+    countHome += 1;
+    homeScore.textContent = countHome;
+}
+
+function plusTwoHome() {
+    countHome += 2;
+    homeScore.textContent = countHome;
+}
+
+function plusThreeHome() {
+    countHome += 3;
+    homeScore.textContent = countHome;
+}
+
+function plusOneGuest() {
+    countGuest += 1;
+    guestScore.textContent = countGuest;
+}
+
+function plusTwoGuest() {
+    countGuest += 2;
+    guestScore.textContent = countGuest;
+}
+
+function plusThreeGuest() {
+    countGuest += 3;
+    guestScore.textContent = countGuest;
+}
